4152ce84d2d9d74023636a9a4658d9e91467fa92
H <heshamscorpe55@gmail.com>
2016-09-14 01:57:18 +0200

    Update json format and use different path

--- server.js
+++ server.js
@@ -2,12 +2,12 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function(request, response) {
+app.get('/api/whoami', function(request, response) {
 
 	const ipaddress = request.get('X-Forwaded-For') || request.connection.remoteAddress;
 	const language = request.get('Accept-Language').split(',')[0];
 	const userAgent = request.get('User-Agent').split(' ');
-	const software = userAgent[1].slice('1') + ' ' + userAgent[2];
+	const software = userAgent[1].slice('1') + ' ' + userAgent[2] + ' ' + userAgent [3].slice(0, -1);
 
 
 	const clientSpecs = {
